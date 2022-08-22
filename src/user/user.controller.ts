d33ff66481bcd0093402e39d96bb737706d422db
gabrielbes-ur <84595525+gabrielbes-ur@users.noreply.github.com>
2022-08-21 21:03:44 -0300

    Redirecionando rota

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Redirect } from '@nestjs/common';
+import { get } from 'https';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor (private userService: UserService) {}
+    @Get()
+    @Redirect('http://localhost:3000/user/new', 302)
+getUser(){
+    return this.userService.getUser();
+}
+
+@Get()
+getUserV2(){
+    return 'Redirected succesfully!';
+}
+
+@Post()
+saveUser(){
+    return 'Created';
+}
+}
+
